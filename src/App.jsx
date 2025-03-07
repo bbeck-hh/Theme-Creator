a6fc50fd4ba1924281bdbc758e9fa49ec8f63c02
Bjoern <beck.b.hh@gmail.com>
2025-03-07 16:31:06 +0100

    Implement dynamic theme(db.js) rendering with Card components and add theme data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
 import Card from "./components/Card/Card";
 import "./styles.css";
+
+import { themes } from './db';
 
 export default function App() {
   return (
     <>
       <h1>Theme Creator</h1>
-      <div class="card-container">
-        <h2 class="card__collection-name">Deep</h2>
-        <Card colorName="deepBlue" />
-        <Card colorName="deepRed" />
-        <Card colorName="deepYellow" />
-        <Card colorName="deepGreen" />
-      </div>
+      {
+        themes.map((theme, index) => (
+          <div key={index} className="card-container">
+            <h2 className="card__collection-name">{theme.name}</h2>
+            {theme.colors.map((color, colorIndex) => (
+              <Card
+                key={colorIndex}
+                colorRole={color.role}
+                colorHex={color.value}
+                colorName={color.name}
+              />
+            ))}
+          </div>
+        ))
+      }
     </>
   );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,15 @@
 import "./Card.css";
 
-export default function Card({ colorName }) {
+export default function Card({ colorRole, colorHex, colorName }) {
     return (
 
         <article className="card">
-            <div className="card__color"></div>
+            <div className="card__color" style={{ backgroundColor: colorHex || '#ffffff' }}><div className="card__color-name">{colorName}</div></div>
             <div className="card__content">
-                <p className="card__color-name">{colorName}</p>
-                <label htmlFor="colorHex"></label>
-                <input type="text" name="colorHex" id="colorHex" />
+                <p className="card__color-role">{colorRole}</p>
+                <label htmlFor="colorHex">{colorHex}</label>
+                <input type="text" name="colorHex" id="colorHex" value={colorHex} readOnly />
+                {/* readOnly muss noch gegen onChange später ersetzt werden */}
             </div>
         </article>
     )
